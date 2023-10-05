8b89e46e61b295c8a6447bcbacbed7d88360bff9
olayyaMansour <mansour.olayya@gmail.com>
2023-10-05 14:24:31 -0700

    Add task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print the given string using the puts function, followed by a newline
+ *
+ * Return: 0 (success, no errors).
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); }
