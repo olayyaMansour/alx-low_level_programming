ec8a7b2f7dd47778016ba7905aa520918022f465
olayyaMansour <mansour.olayya@gmail.com>
2023-10-10 13:19:05 -0700

    Amend task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,34 +5,26 @@
  */
 void times_table(void)
 {
-	int i = 0;
+	int i, j, result;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		int j = 0;
-
-		while (j <= 9)
+		for (j = 0; j <= 9; j++)
 		{
-			int result = i * j;
-
+			result = i * j;
 			if (j == 0)
-			{
-				_putchar(result + '0');
-			}
+				_putchar('0');
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
 				if (result < 10)
 					_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				if (result >= 10)
+					_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
-			j++;
-			if (j <= 9)
-				_putchar(' ');
 		}
 		_putchar('\n');
-		i++;
 	}
 }
