96bad378952b21342ea0af433bb465abebf08c8e
olayyaMansour <mansour.olayya@gmail.com>
2023-10-12 14:21:55 -0700

    Fizz Bazz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints numbers from 1 to 100, replacing multiples of 3 and 5
+ * with "Fizz" and "Buzz" respectively.
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	printf("Fizz");
+	if (i % 5 == 0)
+	printf("Buzz");
+	if (i % 3 != 0 && i % 5 != 0)
+	printf("%d", i);
+
+	if (i != 100)
+	printf(" ");
+	}
+	printf("\n");
+}
+
