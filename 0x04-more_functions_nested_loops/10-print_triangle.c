7540059fa805f374b157f5cb3a620d979ebf73b4
olayyaMansour <mansour.olayya@gmail.com>
2023-10-12 15:11:44 -0700

    bug

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,28 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
 	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-	for (j = 1; j <= i; j++)
-	_putchar('#');
-	_putchar('\n');
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
+
