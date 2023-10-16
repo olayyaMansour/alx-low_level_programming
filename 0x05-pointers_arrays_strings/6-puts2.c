14b5a91d3812df5a9b7e9e218fd89d82e7b56c2b
olayyaMansour <mansour.olayya@gmail.com>
2023-10-16 11:25:20 -0700

    Add task 6 again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int length = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	while (str[length] != '\0')
+		length++;
+
+	for (i = 0; i < length; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 
 	_putchar('\n');
 }
