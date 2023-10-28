20cfc6f9feb990a733f172b234a8d609dd6fa0f3
olayyaMansour <mansour.olayya@gmail.com>
2023-10-27 19:03:28 -0700

    Add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Argument count, the number of command-line arguments
+ * @argv: Argument vector, an array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
