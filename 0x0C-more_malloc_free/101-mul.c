e406aa4c9e114903ce6cfdd6dc44e74cdeab717f
olayyaMansour <mansour.olayya@gmail.com>
2023-11-02 13:17:09 -0700

    Add last task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,40 +1,37 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 for success, 98 for error
+ */
 int main(int argc, char *argv[])
 {
+	char *num1;
+	char *num2;
+	int i;
+	int j;
+	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	char *num1 = argv[1];
-	char *num2 = argv[2];
+	num1 = argv[1];
+	num2 = argv[2];
 
-	for (int i = 0; num1[i] != '\0'; i++)
-	{
-		if (!isdigit(num1[i]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
+	for (i = 0; num1[i] != '\0'; i++)
 
-	for (int i = 0; num2[i] != '\0'; i++)
-	{
-		if (!isdigit(num2[i]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
+	for (j = 0; num2[j] != '\0'; j++)
 
-	int result = atoi(num1) * atoi(num2);
+	result = atoi(num1) * atoi(num2);
 
 	printf("%d\n", result);
-
 	return (0);
 }
 
