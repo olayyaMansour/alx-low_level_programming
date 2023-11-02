2ce8b6cede265fbd18388480af7c96876909f076
olayyaMansour <mansour.olayya@gmail.com>
2023-11-02 13:09:08 -0700

    Add last task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	for (int i = 0; num1[i] != '\0'; i++)
+	{
+		if (!isdigit(num1[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	for (int i = 0; num2[i] != '\0'; i++)
+	{
+		if (!isdigit(num2[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	int result = atoi(num1) * atoi(num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
