a825fb2b5d1f3e1c89e5edd4d484986012f9420e
olayyaMansour <mansour.olayya@gmail.com>
2023-11-06 08:18:35 -0800

    mend task 0 more

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@
  * @owner: Owner of the dog (string)
  * Description: dog struct
  */
+
 struct dog
 {
 	char *name;
@@ -16,10 +17,4 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif /* DOG_H */
