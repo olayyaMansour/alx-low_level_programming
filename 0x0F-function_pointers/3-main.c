f33f4170651264bdf3a8c1b754f6333675b31abd
olayyaMansour <mansour.olayya@gmail.com>
2023-11-08 10:31:31 -0800

    Bug in 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,11 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * main - Prints the opcodes of its own main function.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ *
+ * Return: 0 on success, or an exit code on error.
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
