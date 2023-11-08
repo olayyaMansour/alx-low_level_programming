486fa51c830893c0af8c38812aff97ac2ea7bff8
olayyaMansour <mansour.olayya@gmail.com>
2023-11-08 09:47:20 -0800

    fix buf task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -15,5 +15,10 @@ typedef struct op
 
 
 int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: The operator passed as an argument to the program
