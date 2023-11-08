7dd81f9a3c3e2b609da2ae3aa28aea7e205bae57
olayyaMansour <mansour.olayya@gmail.com>
2023-11-08 10:53:40 -0800

    bug 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,31 +12,22 @@
  */
 int main(int argc, char *argv[])
 {
+	char *p = (char *)main;
+	int num_bytes;
+
 	if (argc != 2)
 	{
-		fprintf(stderr, "Error\n");
-		exit(1);
+		printf("Error\n"), exit(1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
 	if (num_bytes < 0)
 	{
-		fprintf(stderr, "Error\n");
-		exit(2);
+		printf("Error\n"), exit(2);
 	}
 
-	unsigned char *main_ptr = (unsigned char *)main;
-	int i;
-
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x", main_ptr[i]);
-		if (i < num_bytes - 1)
-			printf(" ");
-	}
-	printf("\n");
-
+	while (num_bytes--)
+		printf("%02hhx%s", *p++, num_bytes ? " " : "\n");
 	return (0);
 }
-
