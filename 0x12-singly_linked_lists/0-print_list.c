5698e2682d2ca7f1e85590690bca87fa7d518292
olayyaMansour <mansour.olayya@gmail.com>
2023-11-16 15:49:56 -0800

    Fix betty bug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL) 
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -22,6 +22,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 
-	return count;
+	return (count);
 }
 
