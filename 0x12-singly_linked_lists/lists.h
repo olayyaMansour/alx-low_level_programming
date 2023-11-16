6aada373a5a61d9e02d019fbc35f8e286c696bd7
olayyaMansour <mansour.olayya@gmail.com>
2023-11-16 12:31:32 -0800

    Add first 2 tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,7 +19,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-int _putchar(char c);
+size_t list_len(const list_t *h);
 
-#endif /* LISTS_H */
+#endif
 
