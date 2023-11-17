25065ecb50443d92ac9233f11da35d7a32dc11fa
olayyaMansour <mansour.olayya@gmail.com>
2023-11-16 17:19:57 -0800

    version 111 task 101

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-void myStartupFun(void) __attributr__ ((constructor));
+#include <stdio.h>
+void myStartupFun(void) __attribute__ ((constructor));
 /**
  * myStartupFun - function to be executed before main
  *
@@ -7,7 +8,7 @@ void myStartupFun(void) __attributr__ ((constructor));
  */
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
 
