1986bf68e6a9861dbbc66738a6397c86c184e668
olayyaMansour <mansour.olayya@gmail.com>
2023-11-16 16:55:18 -0800

    fix bug

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * pre_main - Function to be executed before main.
- * Prints "You're beat! and yet, you must allow,\n
- * I bore my house upon my back!\n"
- * before the main function is executed.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void __attribute__((constructor)) pre_main(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n
-		    I bore my house upon my back!\n");
+			I bore my house upon my back!\n");
 }
+
+
 
