baea3464bff5646c92984bfc69ae4777fd25237a
olayyaMansour <mansour.olayya@gmail.com>
2023-11-27 07:24:22 -0800

    Add task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,13 +13,14 @@ void print_error(const char *msg) {
 
 void read_elf_header(int fd) {
     Elf64_Ehdr header;
+    int i;
 
     if (read(fd, &header, sizeof(header)) != sizeof(header))
         print_error("Error reading ELF header");
 
     printf("ELF Header:\n");
     printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
+    for (i = 0; i < EI_NIDENT; i++)
         printf("%02x%c", header.e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
     printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
     printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
