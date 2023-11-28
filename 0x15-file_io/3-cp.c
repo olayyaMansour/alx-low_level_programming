3e0bda63a2f8c0c0394f7334b46ca7935b5d26e4
olayyaMansour <mansour.olayya@gmail.com>
2023-11-27 16:20:09 -0800

    Add task 3 correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,51 +25,31 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+	{ dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+	{ dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		exit(99);
-	}
-
+		exit(99); }
 	while ((rd_bytes = read(fd_from, buffer, BUFSIZE)) > 0)
 	{
 		wr_bytes = write(fd_to, buffer, rd_bytes);
 		if (wr_bytes == -1 || wr_bytes != rd_bytes)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		{ dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
-		}
-	}
-
+			exit(99); } }
 	if (rd_bytes == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+	{ dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
-	}
-
+		exit(98); }
 	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
+	{ dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100); }
 	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return (0);
-}
+	{ dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100); }
+	return (0); }
 
