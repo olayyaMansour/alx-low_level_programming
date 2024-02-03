855a2388a341568f3c80f4cd9f1704974aa73dc9
olayyaMansour <mansour.olayya@gmail.com>
2024-02-02 18:01:55 -0800

    add task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,29 +9,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of hash nodes */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+	/* Allocate memory for the array of hash nodes */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    new_table->size = size;
+	new_table->size = size;
 
-    /* Initialize each element in the array to NULL */
-    for (i = 0; i < size; i++)
-    {
-        new_table->array[i] = NULL;
-    }
+	/* Initialize each element in the array to NULL */
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
 
-    return (new_table);
+	return (new_table);
 }
