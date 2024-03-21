25781423e6a896fbd5d2deeda5a239410c59cd7b
olayyaMansour <mansour.olayya@gmail.com>
2024-03-20 22:07:44 -0700

    add tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    Function to calculate the perimeter of an island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
+
